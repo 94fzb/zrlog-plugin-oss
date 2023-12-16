dca160c4d1df704c8b3cad53e1614aa0a7a0351e
xiaochun <xchun90@163.com>
2023-12-16 16:51:54 +0800

    fix

--- src/main/java/com/zrlog/plugin/oss/timer/SyncTemplateStaticResourceTimerTask.java
+++ src/main/java/com/zrlog/plugin/oss/timer/SyncTemplateStaticResourceTimerTask.java
@@ -5,6 +5,7 @@ import com.hibegin.common.util.FileUtils;
 import com.zrlog.plugin.IMsgPacketCallBack;
 import com.zrlog.plugin.IOSession;
 import com.zrlog.plugin.common.IdUtil;
+import com.zrlog.plugin.common.LoggerUtil;
 import com.zrlog.plugin.common.modle.BlogRunTime;
 import com.zrlog.plugin.common.modle.TemplatePath;
 import com.zrlog.plugin.oss.entry.UploadFile;
@@ -18,12 +19,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SyncTemplateStaticResourceTimerTask extends TimerTask {
 
+    private static final Logger LOGGER = LoggerUtil.getLogger(SyncTemplateStaticResourceTimerTask.class);
+
     private final IOSession session;
 
-    private Map<String, Long> fileWatcherMap = new HashMap<>();
+    private final Map<String, Long> fileWatcherMap = new HashMap<>();
 
     public SyncTemplateStaticResourceTimerTask(IOSession session) {
         this.session = session;
@@ -33,35 +38,37 @@ public class SyncTemplateStaticResourceTimerTask extends TimerTask {
     public void run() {
         Map<String, Object> map = new HashMap<>();
         map.put("key", "syncTemplate");
-        session.sendJsonMsg(map, ActionType.GET_WEBSITE.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, new IMsgPacketCallBack() {
-            @Override
-            public void handler(MsgPacket msgPacket) {
-                Map<String, String> responseMap = new Gson().fromJson(msgPacket.getDataStr(), Map.class);
-                if ("on".equals(responseMap.get("syncTemplate"))) {
-                    TemplatePath templatePath = session.getResponseSync(ContentType.JSON, new HashMap(), ActionType.CURRENT_TEMPLATE, TemplatePath.class);
-                    BlogRunTime blogRunTime = session.getResponseSync(ContentType.JSON, new HashMap(), ActionType.BLOG_RUN_TIME, BlogRunTime.class);
-                    File templateFilePath = new File(blogRunTime.getPath() + templatePath.getValue());
-                    if (templateFilePath.isDirectory()) {
-                        File propertiesFile = new File(templateFilePath + "/template.properties");
-                        if (propertiesFile.exists()) {
-                            Properties prop = new Properties();
-                            try {
-                                prop.load(new FileInputStream(propertiesFile));
-                                String staticResource = (String) prop.get("staticResource");
-                                List<File> fileList = new ArrayList<>();
-                                if (staticResource != null && !"".equals(staticResource)) {
-                                    String[] staticFileArr = staticResource.split(",");
-                                    for (String sFile : staticFileArr) {
-                                        fileList.add(new File(templateFilePath + "/" + sFile));
-                                    }
-                                }
-                                new UploadService().upload(session, convertToUploadFiles(fileList, blogRunTime.getPath()));
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
+        session.sendJsonMsg(map, ActionType.GET_WEBSITE.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, msgPacket -> {
+            Map<String, String> responseMap = new Gson().fromJson(msgPacket.getDataStr(), Map.class);
+            if (!"on".equals(responseMap.get("syncTemplate"))) {
+                return;
+            }
+            TemplatePath templatePath = session.getResponseSync(ContentType.JSON, new HashMap<>(), ActionType.CURRENT_TEMPLATE, TemplatePath.class);
+            BlogRunTime blogRunTime = session.getResponseSync(ContentType.JSON, new HashMap<>(), ActionType.BLOG_RUN_TIME, BlogRunTime.class);
+            File templateFilePath = new File(blogRunTime.getPath() + templatePath.getValue());
+            if (!templateFilePath.isDirectory()) {
+                LOGGER.log(Level.INFO,"Template path not directory " + templateFilePath);
+            }
+            File propertiesFile = new File(templateFilePath + "/template.properties");
+            if(!propertiesFile.exists()){
+                LOGGER.log(Level.SEVERE,"Template properties error " + templateFilePath);
+
+                return;
+            }
+            Properties prop = new Properties();
+            try (FileInputStream fileInputStream = new FileInputStream(propertiesFile)){
+                prop.load(fileInputStream);
+                String staticResource = (String) prop.get("staticResource");
+                List<File> fileList = new ArrayList<>();
+                if (staticResource != null && !staticResource.isEmpty()) {
+                    String[] staticFileArr = staticResource.split(",");
+                    for (String sFile : staticFileArr) {
+                        fileList.add(new File(templateFilePath + "/" + sFile));
                     }
                 }
+                new UploadService().upload(session, convertToUploadFiles(fileList, blogRunTime.getPath()));
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE,"",e);
             }
         });
 
