06f5ef003e861ac91add707b110360d526c5a519
xiaochun <xchun90@163.com>
2024-04-25 10:33:36 +0800

    Use crc32

--- src/main/java/com/zrlog/plugin/oss/timer/SyncTemplateStaticResourceTimerTask.java
+++ src/main/java/com/zrlog/plugin/oss/timer/SyncTemplateStaticResourceTimerTask.java
@@ -8,6 +8,7 @@ import com.zrlog.plugin.common.LoggerUtil;
 import com.zrlog.plugin.common.modle.BlogRunTime;
 import com.zrlog.plugin.common.modle.TemplatePath;
 import com.zrlog.plugin.data.codec.ContentType;
+import com.zrlog.plugin.data.codec.MsgPacket;
 import com.zrlog.plugin.data.codec.MsgPacketStatus;
 import com.zrlog.plugin.oss.FileUtils;
 import com.zrlog.plugin.oss.entry.UploadFile;
@@ -35,48 +36,74 @@ public class SyncTemplateStaticResourceTimerTask extends TimerTask {
         this.session = session;
     }
 
+    private List<UploadFile> cacheFiles(BlogRunTime blogRunTime) {
+        String cacheFolder = new File(blogRunTime.getPath()).getParent() + "/cache/zh_CN";
+        File cacheFile = new File(cacheFolder);
+        List<UploadFile> uploadFiles = new ArrayList<>();
+        if (cacheFile.exists()) {
+            File[] fs = cacheFile.listFiles();
+            uploadFiles.addAll(convertToUploadFiles(Arrays.asList(fs), cacheFolder));
+        }
+        return uploadFiles;
+    }
+
+    private List<UploadFile> templateUploadFiles(BlogRunTime blogRunTime, Map<String, String> responseMap, TemplatePath templatePath) {
+        if (!"on".equals(responseMap.get("syncTemplate"))) {
+            return new ArrayList<>();
+        }
+        File templateFilePath = new File(blogRunTime.getPath() + templatePath.getValue());
+        if (!templateFilePath.isDirectory()) {
+            LOGGER.log(Level.INFO, "Template path not directory " + templateFilePath);
+            return new ArrayList<>();
+        }
+        File propertiesFile = new File(templateFilePath + "/template.properties");
+        if (!propertiesFile.exists()) {
+            LOGGER.log(Level.SEVERE, "Template properties error " + templateFilePath);
+            return new ArrayList<>();
+        }
+        List<UploadFile> uploadFiles = new ArrayList<>();
+        Properties prop = new Properties();
+
+        try (FileInputStream fileInputStream = new FileInputStream(propertiesFile)) {
+            prop.load(fileInputStream);
+            String staticResource = (String) prop.get("staticResource");
+            List<File> fileList = new ArrayList<>(getStaticFolderFiles(staticResource, templateFilePath, blogRunTime));
+            uploadFiles.addAll(convertToUploadFiles(fileList, blogRunTime.getPath()));
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "", e);
+        }
+        return uploadFiles;
+    }
+
     @Override
     public void run() {
         Map<String, Object> map = new HashMap<>();
-        map.put("key", "syncTemplate,access_key,secret_key,host,region,supportHttps");
+        String cacheKey = "_cacheInfo";
+        map.put("key", "syncTemplate,access_key,secret_key,host,region,supportHttps," + cacheKey);
         session.sendJsonMsg(map, ActionType.GET_WEBSITE.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, msgPacket -> {
             Map<String, String> responseMap = new Gson().fromJson(msgPacket.getDataStr(), Map.class);
-            if (!"on".equals(responseMap.get("syncTemplate"))) {
-                return;
+            String cacheStr = responseMap.get(cacheKey);
+            if (Objects.nonNull(cacheStr) && !cacheStr.trim().isEmpty()) {
+                fileWatcherMap.putAll(new Gson().fromJson(cacheStr, Map.class));
             }
             TemplatePath templatePath = session.getResponseSync(ContentType.JSON, new HashMap<>(), ActionType.CURRENT_TEMPLATE, TemplatePath.class);
             BlogRunTime blogRunTime = session.getResponseSync(ContentType.JSON, new HashMap<>(), ActionType.BLOG_RUN_TIME, BlogRunTime.class);
-            File templateFilePath = new File(blogRunTime.getPath() + templatePath.getValue());
-            if (!templateFilePath.isDirectory()) {
-                LOGGER.log(Level.INFO, "Template path not directory " + templateFilePath);
-            }
-            File propertiesFile = new File(templateFilePath + "/template.properties");
-            if (!propertiesFile.exists()) {
-                LOGGER.log(Level.SEVERE, "Template properties error " + templateFilePath);
+            List<UploadFile> uploadFiles = new ArrayList<>();
+            uploadFiles.addAll(templateUploadFiles(blogRunTime, responseMap, templatePath));
+            uploadFiles.addAll(cacheFiles(blogRunTime));
+            if (uploadFiles.isEmpty()) {
                 return;
             }
-            Properties prop = new Properties();
-            try (FileInputStream fileInputStream = new FileInputStream(propertiesFile)) {
-                prop.load(fileInputStream);
-                String staticResource = (String) prop.get("staticResource");
-                List<File> fileList = getStaticFolderFiles(staticResource, templateFilePath, blogRunTime);
-                List<UploadFile> uploadFiles = convertToUploadFiles(fileList, blogRunTime.getPath());
-                String cacheFolder = new File(blogRunTime.getPath()).getParent() + "/cache/zh_CN";
-                File cacheFile = new File(cacheFolder);
-                if (cacheFile.exists()) {
-                    File[] fs = cacheFile.listFiles();
-                    uploadFiles.addAll(convertToUploadFiles(Arrays.asList(fs), cacheFolder));
-                }
-                new UploadService().upload(session, uploadFiles);
-                new PreFetchCdnWorker(responseMap.get("access_key"), responseMap.get("secret_key"),
-                        responseMap.get("region"), responseMap.get("host"),
-                        Objects.equals("on", responseMap.get("supportHttps")), uploadFiles.stream().map(UploadFile::getFileKey)
-                        .collect(Collectors.toList())).run();
-            } catch (IOException e) {
-                LOGGER.log(Level.SEVERE, "", e);
-            }
+            new UploadService().upload(session, uploadFiles);
+            Map<String, String> hashMap = new HashMap<>();
+            hashMap.put("_cacheInfo", new Gson().toJson(fileWatcherMap));
+            session.sendMsg(new MsgPacket(hashMap, ContentType.JSON, MsgPacketStatus.SEND_REQUEST, IdUtil.getInt(), ActionType.SET_WEBSITE.name()), x -> {
+                Map<String, Object> rmap = new HashMap<>();
+                rmap.put("success", true);
+                session.sendMsg(new MsgPacket(map, ContentType.JSON, MsgPacketStatus.RESPONSE_SUCCESS, x.getMsgId(), x.getMethodStr()));
+            });
+            new PreFetchCdnWorker(responseMap.get("access_key"), responseMap.get("secret_key"), responseMap.get("region"), responseMap.get("host"), Objects.equals("on", responseMap.get("supportHttps")), uploadFiles.stream().map(UploadFile::getFileKey).collect(Collectors.toList())).run();
         });
-
     }
 
     private static List<File> getStaticFolderFiles(String staticResource, File templateFilePath, BlogRunTime blogRunTime) {
