38650412ff259eb59e6c81a1a8ea0a7de130f703
xiaochun <xchun90@163.com>
2024-05-28 15:53:29 +0800

    to Sync

---
+++ bin/package-native-github.sh
@@ -0,0 +1,7 @@
+java -version
+./mvnw clean package
+./mvnw -Pnative -Dagent exec:exec@java-agent -U
+./mvnw -Pnative package
+basePath=/tmp/download/plugin
+mkdir -p ${basePath}
+mv target/changyan ${basePath}/changyan-$(uname -s)-$(uname -m).bin
---
+++ src/main/java/com/zrlog/plugin/oss/GraalvmAgentApplication.java
@@ -0,0 +1,28 @@
+package com.zrlog.plugin.oss;
+
+import com.google.gson.Gson;
+import com.zrlog.plugin.common.PluginNativeImageUtils;
+import com.zrlog.plugin.data.codec.HttpRequestInfo;
+import com.zrlog.plugin.message.Plugin;
+import com.zrlog.plugin.oss.controller.OssController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+public class GraalvmAgentApplication {
+
+
+    public static void main(String[] args) throws IOException {
+        new Gson().toJson(new HttpRequestInfo());
+        new Gson().toJson(new Plugin());
+        String basePath = System.getProperty("user.dir").replace("/target", "");
+        //PathKit.setRootPath(basePath);
+        File file = new File(basePath + "/src/main/resources");
+        PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath(), "/");
+        //Application.nativeAgent = true;
+        PluginNativeImageUtils.exposeController(Collections.singletonList(OssController.class));
+        Application.main(args);
+
+    }
+}
