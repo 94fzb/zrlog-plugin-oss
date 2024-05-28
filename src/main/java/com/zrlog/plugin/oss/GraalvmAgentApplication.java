0e65e065707da309bfb878c63ce2bc47fb59a5a9
xiaochun <xchun90@163.com>
2024-05-28 16:12:00 +0800

    to Sync

--- src/main/java/com/zrlog/plugin/oss/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/oss/GraalvmAgentApplication.java
@@ -5,6 +5,7 @@ import com.zrlog.plugin.common.PluginNativeImageUtils;
 import com.zrlog.plugin.data.codec.HttpRequestInfo;
 import com.zrlog.plugin.message.Plugin;
 import com.zrlog.plugin.oss.controller.OssController;
+import com.zrlog.plugin.oss.service.UploadService;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,9 +14,10 @@ import java.util.Collections;
 public class GraalvmAgentApplication {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException {
         new Gson().toJson(new HttpRequestInfo());
         new Gson().toJson(new Plugin());
+        UploadService.class.newInstance();
         String basePath = System.getProperty("user.dir").replace("/target", "");
         //PathKit.setRootPath(basePath);
         File file = new File(basePath + "/src/main/resources");
